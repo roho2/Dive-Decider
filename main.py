db9124e288a59ac3dc5d905fa5f29226384090c7
roho2 <rjhollinger@gmail.com>
2023-09-16 01:11:17 -0400

    Added a few notes to try and remember for later. Also added a loop to go through the json object.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 # stormglass.io API key - f23856c2-543b-11ee-a654-0242ac130002-f2385780-543b-11ee-a654-0242ac130002 (10 requests/day)
 # I want to create a program that allows you to choose a dive site and get back a response that says
 # If it is a good day to dive or not based on data from stormglass.io. It will provide surf conditions.
+
+# Tech ideas, Django for web. MySQL or PostgreSQL to store data (dive sites?, even users info (fav dive sites?)).
+# Django ORM help database operations by allowing you to interact using python objects
+# Host on AWS or Heroku?
+# OAuth or JWT for authentication. pytest to test code?
+
+# I'm tired, it's 1:09am. Jump back into this tomorrow. This will be a long project but worth it. Start: 9/15/2023
+# Spent about 1 hour so far
 import requests
 import arrow
 
