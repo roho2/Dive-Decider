ce6262c941098f953fb96df735e0880da5bce4ec
roho2 <rjhollinger@gmail.com>
2023-09-16 00:19:06 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# stormglass.io API key - f23856c2-543b-11ee-a654-0242ac130002-f2385780-543b-11ee-a654-0242ac130002
+# I want to create a program that allows you to choose a dive site and get back a response that says
+# If it is a good day to dive or not based on data from stormglass.io. It will provide surf conditions.
+
+if __name__ == "__main__":
+    print("Hello world!")
