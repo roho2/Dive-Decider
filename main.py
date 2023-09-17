739bf6c0b63baafe3a9daa18b9c8ef43de9aced7
roho2 <rjhollinger@gmail.com>
2023-09-17 00:22:39 -0400

    Switched the section of the API I was working with from the tide area to the Weather. It has more data we need

--- main.py
+++ main.py
@@ -7,39 +7,51 @@
 # Host on AWS or Heroku?
 # OAuth or JWT for authentication. pytest to test code?
 
-# I'm tired, it's 1:09am. Jump back into this tomorrow. This will be a long project but worth it. Start: 9/15/2023
-# Spent about 1 hour so far
+# If I were to do an app, I would probably do it all with just AWS and Python Lambdas lol
+# Run a SUPER light app
+# You can use API gateway to handle the API calls and routing, DynamoDB for your database, and just use Python Lambdas to handle your backend service code
+
+# Start: 9/15/2023
+# Spent about 3 hours so far
+
 import requests
 import arrow
 import json
 import pprint
 
 start = arrow.now().floor('day')
-end = arrow.now().shift(days=1).floor('day')
-
-url = 'https://api.stormglass.io/v2/tide/extremes/point'
-params = {
-            'lat': 26.2055,
-            'lng': -80.0851,
-            'start': start.to('UTC').timestamp(),  # Convert to UTC timestamp
-            'end': end.to('UTC').timestamp(),  # Convert to UTC timestamp
-        }
-headers = {
-            'Authorization': 'f23856c2-543b-11ee-a654-0242ac130002-f2385780-543b-11ee-a654-0242ac130002'
-        }
+end = arrow.now().ceil('day')
 
 if __name__ == "__main__":
-    start = arrow.now().floor('day')
-    end = arrow.now().shift(days=1).floor('day')
 
-    response = requests.get(url, params, headers)
+    response = requests.get(
+        'https://api.stormglass.io/v2/weather/point',
+        params={
+            'lat': 26.2055,
+            'lng': -80.0851,
+            'params': ['waveHeight', 'waterTemperature', 'wavePeriod', 'windWaveHeight', 'windSpeed',
+                       'currentDirection', 'currentSpeed', 'visibility'],
+            'start': start.to('UTC').timestamp(),  # Convert to UTC timestamp
+            'end': end.to('UTC').timestamp(),  # Convert to UTC timestamp
+            # waveHeight is in meters
+            # It's only printing waveHeight for now, ugh.
+        },
+        headers={
+            'Authorization': 'f23856c2-543b-11ee-a654-0242ac130002-f2385780-543b-11ee-a654-0242ac130002'
+        }
+    )
+
     if response.status_code == 200:
-        json_data = json.loads(response.text)
-        pprint(json_data)
+        json_data_response = response.json()
+        print(json_data_response)
+        print("-------------------------------------------------------")
+        json_data = json.loads(response.text)  # json_data is a dictionary
+        pprint.pprint(json_data)
+        # as an example of how to get the data (this is not real variable names from API
+        # name = json_data['name']
+        # print(f"{name} is the name")
     else:
         print(f"Error:  {response.status_code}")  # Formatted string literal. contain replacement filed
 
-
     # for i in json_data:
     #     print(i, ":", json_data[i])
-
