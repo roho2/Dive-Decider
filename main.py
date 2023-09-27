41ff83de308010fecef4402a80b19925ec9ed54f
roho2 <rjhollinger@gmail.com>
2023-09-27 17:27:31 -0400

    Got background working and favicon. Basic things coming together!

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 # stormglass.io API key - f23856c2-543b-11ee-a654-0242ac130002-f2385780-543b-11ee-a654-0242ac130002 (10 requests/day)
-# I want to create a program that allows you to choose a dive site and get back a response that says
-# If it is a good day to dive or not based on data from stormglass.io. It will provide surf conditions.
 
 # Host on AWS or Heroku?
 # OAuth or JWT for authentication. pytest to test code?
